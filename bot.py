e9067b436f0faf2607499fa7772dd39b1cfe9b9a
Kyryl Dubovetskyi <krupartea@gmail.com>
2024-05-21 00:43:51 +0200

    Make logging work

--- bot.py
+++ bot.py
@@ -4,14 +4,43 @@ from pedalboard import Pedalboard, Reverb, time_stretch
 from pedalboard.io import AudioFile
 import io
 import pedalboard
+from telebot import types
+import pickle
+from pathlib import Path
+import os
+
+
+MESSAGE_PICKLES_DIR = Path(r"message_pickles")
+LOG_PATH = Path(r"message_log.csv")
+
+
+telebot.apihelper.ENABLE_MIDDLEWARE = True
 
 
 bot = telebot.TeleBot(TG_TOKEN, parse_mode=None)
 
 
+# read the log file
+# columns are: message_id,user_id,timestamp,pickle_path
+message_log = open(LOG_PATH, "a")
+
+
+@bot.middleware_handler(update_types=['message'])
+def pickle_message(bot_instance, message):
+    path = MESSAGE_PICKLES_DIR / f"id{message.id}_userid{message.from_user.id}_date{message.date}.pickle"
+    with open(path, "wb") as f:
+        pickle.dump(message, f)
+    
+    # log a subset of message attributes to a .csv file
+    message_log.write(f"{message.id},{message.from_user.id},{message.date},{path}\n")
+    message_log.flush()  # moves (flushes) the runtime file buffer to OS
+    os.fsync(message_log.fileno())
+
+
 @bot.message_handler(commands=["start"])
 def welcome(message):
-    bot.reply_to(message, "Welcome to the Slowed and Reverbed world!\nSend an .mp3 file to get an improved version ;)")
+    bot.reply_to(message, "Welcome to the Slowed and Reverbed world!\nSend an .mp3 file to get its improved version ;)")
+
 
 @bot.message_handler(content_types=["audio"])
 def process_audio(message):
@@ -25,21 +54,20 @@ def process_audio(message):
     file_alike.write(bytes)
     file_alike.seek(0)  # now f is feedable to Pedalboard's AudioFile
 
+    # TODO: check if the file is valid (e.g. if the maximum sendable filesize
+    # is not exceeded)
+
+
+    # set slowing and reverberation parameters
+
+
     with AudioFile(file_alike, "r") as ain:
-
-        # original_sample_rate = ain.samplerate
-
-        # # stretch
-        # stretch_factor = 1.20
-        # new_sample_rate = original_sample_rate / stretch_factor
-        # ain = ain.resampled_to(new_sample_rate)
-
         buffer = io.BytesIO()
         with AudioFile(buffer, "w", ain.samplerate, ain.num_channels, format="mp3") as aout:
             # read one second of audio at a time, until the file is empty:
             while ain.tell() < ain.frames:
                 chunk = ain.read(ain.samplerate)
-                
+
                 # slow down
                 # TODO: research on a time stretching which doesn't compesnate
                 # pitch implicitly
@@ -59,3 +87,4 @@ def process_audio(message):
 
 
 bot.infinity_polling()
+message_log.close()
