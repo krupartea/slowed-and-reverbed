6b2121554cd9580167c2605ce08e16dc557a564f
Kyryl Dubovetskyi <krupartea@gmail.com>
2024-05-22 19:13:39 +0200

    the only thing is left here is to handle the filename from the prev message for the processing input

--- bot.py
+++ bot.py
@@ -26,10 +26,16 @@ bot = telebot.TeleBot(TG_TOKEN, parse_mode=None)
 message_log = open(LOG_PATH, "a")
 
 
-# create the user-state dict
+# create the user-info dict
 # TODO: store on the drive
-# keys: user_id, values: user_state (control flow state)
-user_state = {}
+# dictionary structure:
+# {
+#     "user_id":{
+#         "mode": "default",
+#         "stage": "home",
+#     }
+# }
+user_info = {}
 # POSSIBLE STATES: "default", "setting_stretch", "setting_reverb", "advanced"
 
 
@@ -54,8 +60,11 @@ def pickle_message(bot_instance, message):
 
 @bot.middleware_handler(update_types=['message'])
 def new_user(bot_instance, message):
-    if message.from_user.id not in user_state:  # new user case
-        user_state[message.from_user.id] = "default"
+    if message.from_user.id not in user_info:  # new user case
+        user_info[message.from_user.id] = {
+            "mode": "default",
+            "stage": "home",
+        }
         welcome(message)
 
 
@@ -66,7 +75,7 @@ def welcome(message):
 
 @bot.message_handler(commands=["default", "advanced"])
 def set_mode(message):
-    user_state[message.from_user.id] = message.text[1:]
+    user_info[message.from_user.id]["mode"] = message.text[1:]
     if message.text[1:] == "advanced":
         bot.send_message(message, "In the advanced mode you can set a custom stretching factor and the amount of reverberation.\nBut first, send the .mp3 file.")
     else:
@@ -75,23 +84,75 @@ def set_mode(message):
 
 
 @bot.message_handler(content_types=["audio"],
-                     func=lambda m: user_state[m.from_user.id] in ["default", "advanced"])
-def process_audio(message):
-    file_info = bot.get_file(message.audio.file_id)
-    audio_bytes = bot.download_file(file_info.file_path)
+                     func=lambda m: (user_info[m.from_user.id]["mode"] in ["default", "advanced"])\
+                        (user_info[m.from_user.id]["stage"] == "home"))
+def receive_audio(message):
+    if user_info[message.from_user.id]["mode"] == "default":
+        # TODO: check if the file is valid (e.g. if the maximum sendable filesize
+        # is not exceeded)
 
-
-    # TODO: check if the file is valid (e.g. if the maximum sendable filesize
-    # is not exceeded)
-
-
-    # set slowing and reverberation parameters
-
-    if user_state[message.from_user.id] == "default":
+        # TODO: update typing status to "Sending audio..."
+        # and let the user know how long will it take
+        file_info = bot.get_file(message.audio.file_id)
+        audio_bytes = bot.download_file(file_info.file_path)
         output_buffer = slow_and_reverb(audio_bytes)
         bot.send_audio(message.chat.id, telebot.types.InputFile(output_buffer))
-    else:
-        pass
+    else:  # advanced mode
+        # start setting reverb and slowing params
+
+        # construct the keyboard with valid slowing options
+        markup = types.ReplyKeyboardMarkup()
+        markup.add(
+            types.KeyboardButton(r'10%'),
+            types.KeyboardButton(r'25%'),
+            types.KeyboardButton(r'50%'),
+        )
+
+        bot.send_message(message, "Select how much to slow down the audio",
+                         reply_markup=markup)
+        user_info[message.from_user.id]["stage"] = "selecting_slowing"
+
+
+@bot.message_handler(content_types=["audio"],
+                     func=lambda m: (user_info[m.from_user.id]["mode"] == "advanced")\
+                        and (user_info[m.from_user.id]["stage"] == "selecting_slowing"))
+def set_slowing(message):
+    # TODO: assert the input format is correct e.g. '25%'
+    # set user slowing rate as user's param
+    user_info[message.from_user.id]["slowing"] = (100 - int(message.text[:-1])) / 100
+
+    # suggest user to select reverb options
+    # construct the keyboard with valid reverb options
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(
+        types.KeyboardButton(r'10%'),
+        types.KeyboardButton(r'50%'),
+        types.KeyboardButton(r'100%'),
+    )
+
+    bot.send_message(message, "Select how much reverb do you want",
+                        reply_markup=markup)
+    user_info[message.from_user.id]["stage"] = "selecting_reverb"
+
+
+@bot.message_handler(content_types=["audio"],
+                     func=lambda m: (user_info[m.from_user.id]["mode"] == "advanced")\
+                        and (user_info[m.from_user.id]["stage"] == "selecting_slowing"))
+def set_reverb(message):
+    # TODO: assert the input format is correct e.g. '25%'
+    # set user slowing rate as user's param
+    user_info[message.from_user.id]["reverb"] = (100 - int(message.text[:-1])) / 100
+
+    # now both slowing rate and the amount of reverb are set, we can
+    # process the audio
+
+    # obtain the message object in which the audio file was sent
+
+    file_info = bot.get_file(message.audio.file_id)
+    audio_bytes = bot.download_file(file_info.file_path)
+    output_buffer = slow_and_reverb(audio_bytes)
+    bot.send_audio(message.chat.id, telebot.types.InputFile(output_buffer))
+
 
 
 bot.infinity_polling()
